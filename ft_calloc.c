df35d291c6571a33900afbdf27ae6b271efe1969
kfujita <kfujita@student.42tokyo.jp>
2022-04-19 05:23:17 +0900

    Add overflow handling

--- ft_calloc.c
+++ ft_calloc.c
@@ -6,7 +6,7 @@
 /*   By: kfujita <kfujita@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/18 03:15:08 by kfujita           #+#    #+#             */
-/*   Updated: 2022/04/18 07:42:11 by kfujita          ###   ########.fr       */
+/*   Updated: 2022/04/19 05:22:23 by kfujita          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,10 +20,10 @@ void	*ft_calloc(size_t count, size_t size)
 	size_t	bytes_to_allocate;
 
 	bytes_to_allocate = count * size;
-	if ((double)count * (double)size > SIZE_T_MAX)
-		return (NULL);
 	if (bytes_to_allocate <= 0)
 		bytes_to_allocate = 1;
+	if (bytes_to_allocate < count && bytes_to_allocate < size)
+		return (NULL);
 	p_ret = malloc(bytes_to_allocate);
 	if (p_ret != NULL)
 		ft_bzero(p_ret, bytes_to_allocate);
